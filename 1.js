27b7606d14a91943e37457e5f61e0cd54c9e43b1
Dmitry Fisunov <solarman.awm@gmail.com>
2026-04-01 01:54:44 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,14 @@
+import OrderRepository from './OrderRepository';
+import EmailSender from './EmailSender';
+
+class OrderService {
+  constructor() {
+    this.orderRepository = new OrderRepository();
+    this.emailSender = new EmailSender();
+  }
+
+  createOrder(order) {
+    this.orderRepository.create(order);
+    this.emailSender.send(order);
+  }
+}
